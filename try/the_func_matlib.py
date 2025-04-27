4b257027bb8ed03dbdb9938c03088e6d532e5fcf
yousra-n <155311241+yousra-n@users.noreply.github.com>
2025-04-27 15:04:20 +0100

    Update the_func_matlib.py
    added some visuals fixed some stuff

--- try/the_func_matlib.py
+++ try/the_func_matlib.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import mplcursors
 from scipy.signal import savgol_filter
+from mpl_toolkits.mplot3d import Axes3D
+
 
 def file_reader(file_name): #to read the file 
     data = []
@@ -12,6 +15,13 @@ def file_reader(file_name): #to read the file
             except ValueError:
                 pass
     return data
+
+
+def Normal(data): #idk dont do much 
+        max_index = np.argmax(data) # find index of max value
+        data[0], data[max_index] = data[max_index], data[0]  # swap
+        return data
+ 
 
 def Fix_Max(data): #get the max value and fix the values
     half_length = len(data) // 2
@@ -25,32 +35,44 @@ def Fix_Max(data): #get the max value and fix the values
 
     return plan_e_2d, plan_h_2d #returns 2lists of the values 
 
-def plot_2d(values, max=True, same=True): #to draw the graph 
+
+def plot_2d(values, max=True, same=True):  # to draw the graph
     half_length = len(values) // 2
     theta_deg = np.linspace(0, 360, half_length)
     theta_rad = np.radians(theta_deg)
 
-    if max: #to switch the values
+    if max:  # switch the value max 
         plan_e_2d, plan_h_2d = Fix_Max(values)
+        np1 = Normal(plan_e_2d) #so swasp the index
+        np2 = Normal(plan_h_2d)
+        
+        # filter the values
+        np1_filtered = savgol_filter(np1, window_length=11, polyorder=2)
+        np2_filtered = savgol_filter(np2, window_length=11, polyorder=2)
+        
         if same:
-            Afficher(plan_e_2d, plan_h_2d, theta_rad, same=True)
+            Afficher_2d(np1_filtered, np2_filtered, theta_rad, same=True)
         else:
-            Afficher(plan_e_2d, plan_h_2d, theta_rad, same=False)
-    else: #we dont 
+            Afficher_2d(np1_filtered, np2_filtered, theta_rad, same=False)
+
+    else:  # no max change
         plan_e = np.array(values[:half_length])
         plan_h = np.array(values[half_length:])
 
-        plan_h_db = savgol_filter(plan_e, window_length=11, polyorder=2)
-        plan_e_db = savgol_filter(plan_h, window_length=11, polyorder=2)
+        plan_e_filtered = savgol_filter(plan_e, window_length=11, polyorder=2)
+        plan_h_filtered = savgol_filter(plan_h, window_length=11, polyorder=2)
 
-        if same: #the'll have the same place
-            Afficher(plan_e_db, plan_h_db, theta_rad, same=True)
-        else:  #diffrent place
-            Afficher(plan_e_db, plan_h_db, theta_rad, same=False)
+        if same:
+            Afficher_2d(plan_e_filtered, plan_h_filtered, theta_rad, same=True)
+        else:
+            Afficher_2d(plan_e_filtered, plan_h_filtered, theta_rad, same=False)
 
-def Afficher(plan_e_2d, plan_h_2d, theta, same=True): # the affichage fonction 
 
-    if same: #of ots on the same 
+def Afficher_2d(plan_e_2d, plan_h_2d, theta, same=True): #affichage fonction
+    fig, ax = None, None
+
+    if same: #si les deux graphe in the same plot
+        # Create the same plot
         fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
         ax.plot(theta, plan_h_2d, label='H-plane', color='blue')
         ax.plot(theta, plan_e_2d, label='E-plane', color='red')
@@ -60,31 +82,48 @@ def Afficher(plan_e_2d, plan_h_2d, theta, same=True): # the affichage fonction
         ax.legend(loc='upper right')
         fig.set_size_inches(6, 6)
 
-    else: # seperated
+        #to get the values like plotly bl curser (can make it custom)
+        cursor = mplcursors.cursor([ax], hover=True)
+        cursor.connect("add", lambda sel: sel.annotation.set_text(f"Angle: {np.degrees(sel.target[0]):.1f}°\nValue: {sel.target[1]:.2f} dB"))
+
+    else: #if les deux graphe they on seperat plot 
+        # Create separate plots
         fig, (ax1, ax2) = plt.subplots(1, 2, subplot_kw={'projection': 'polar'}, figsize=(12, 6))
+
         ax1.plot(theta, plan_h_2d, label='H-plane', color='blue')
         ax1.set_title("H-plane Pattern")
         ax1.set_theta_direction(-1)
         ax1.set_theta_offset(np.pi / 2)
+        ax1.legend(loc='upper right')
 
         ax2.plot(theta, plan_e_2d, label='E-plane', color='red')
         ax2.set_title("E-plane Pattern")
         ax2.set_theta_direction(-1)
         ax2.set_theta_offset(np.pi / 2)
+        ax2.legend(loc='upper right')
 
         fig.suptitle("2D Radiation Patterns - Separate", fontsize=16)
 
+        #to get the values like plotly bl curser (can make it custom)
+        cursor = mplcursors.cursor([ax1, ax2], hover=True)
+        cursor.connect("add", lambda sel: sel.annotation.set_text(f"Angle: {np.degrees(sel.target[0]):.1f}°\nValue: {sel.target[1]:.2f} dB"))
+
     plt.tight_layout()
     plt.show()
-    
-def plot_3d(data):
+
+
+def plot_3d(data, max=True):
     half_length = len(data) // 2
     plan_e = np.array(data[:half_length])
     plan_h = np.array(data[half_length:])
+    
+   
+    if max: #if we do the max thing 
+        plan_e_db, plan_h_db = Fix_Max(data)  
 
-    # Smooth data
-    plan_h_db = savgol_filter(plan_e, 11, 2)
-    plan_e_db = savgol_filter(plan_h, 11, 2)
+    else: #no max thing
+        plan_h_db = savgol_filter(plan_e, 11, 2)
+        plan_e_db = savgol_filter(plan_h, 11, 2)
 
     # Convert theta to radians
     theta = np.linspace(0, 2 * np.pi, half_length)
@@ -92,23 +131,45 @@ def plot_3d(data):
     theta, phi = np.meshgrid(theta, phi)
 
     # Fake 3D radial values by averaging E & H
-    r = (np.outer(plan_h_db, np.ones_like(plan_e_db)) +
-         np.outer(np.ones_like(plan_h_db), plan_e_db)) / 2
+    r = (np.outer(plan_h_db, np.ones_like(plan_e_db)) + np.outer(np.ones_like(plan_h_db), plan_e_db)) / 2
 
     # Convert spherical to cartesian
     X = r * np.sin(phi) * np.cos(theta)
     Y = r * np.sin(phi) * np.sin(theta)
     Z = r * np.cos(phi)
 
+    # Create the figure and the 3D axis
     fig = plt.figure(figsize=(10, 8))
     ax = fig.add_subplot(111, projection='3d')
+
+    # Plot the surface
     surf = ax.plot_surface(X, Y, Z, cmap='plasma', linewidth=0.5)
 
     ax.set_title('3D Radiation Pattern (E + H averaged)', fontsize=14)
+
+    # to get like the values printed (kinda like plotly but with click)
     fig.colorbar(surf, shrink=0.6, aspect=10)
+    #the box that show the value 
+    annotation_box = ax.text2D(0.05, 0.9, '', transform=ax.transAxes, fontsize=12, va='center', ha='left', bbox=dict(facecolor='lightblue', alpha=0.5, edgecolor='black'))
+
+    def on_click(event): # the event of the click so that it shows
+        if event.inaxes == ax: #check click in or out the plot 
+            x_click, y_click = event.xdata, event.ydata #to get the coordenet
+
+            distance = np.sqrt((X - x_click)**2 + (Y - y_click)**2)  # to get the nerest value
+            min_dist_index = np.unravel_index(np.argmin(distance), distance.shape)
+
+            angle = np.degrees(theta[min_dist_index])
+            value = Z[min_dist_index]
+
+            annotation_box.set_text(f"Angle: {angle:.1f}°\nValue: {value:.2f} dB" ) #update the vla in the box 
+            plt.draw() #to read draw the plot with the box 
+
+    fig.canvas.mpl_connect('button_press_event', on_click) # to work (conest the bo=utton click the the event click)(kayen click the fuc is called)
+
     plt.show()
-    
+
 # Example of a main 
-data = file_reader("3Dcourbe4.txt")
-plot_2d(data, max=False, same=True)
-plot_3d(data)
+data = file_reader("3Dcourbe2.txt")
+plot_2d(data, max=True , same=False)
+#plot_3d(data)
